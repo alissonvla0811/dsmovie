a24a6945d68c4bb50953a9ed8085b1e023aa7e5c
Alisson Vieira <alissonvla@gmail.com>
2022-01-10 19:55:54 -0300

    correcao problema imagem

--- frontend/src/components/NavBar/index.tsx
+++ frontend/src/components/NavBar/index.tsx
@@ -1,6 +1,6 @@
 import "./styles.css";
 
-//import {ReactComponent as GithubIcon} from 'assets/img/github.svg';
+import {ReactComponent as GithubIcon} from 'assets/img/github.svg';
 
 function NavBar() {
     return (
@@ -10,6 +10,7 @@ function NavBar() {
                     <h1>DSMovie</h1>
                     <a href="https://github.com/devsuperior" target="_blank" rel="noreferrer">
                         <div className="dsmovie-contact-container">
+                            <GithubIcon/>
                             <p className="dsmovie-contact-link">/devsuperior</p>
                         </div>
                     </a>
